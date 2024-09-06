a1e9b53cf89fe5bfdd5c8224ef7af5223f2742e4
LucasDuMartinez <133716670+LucasDuMartinez@users.noreply.github.com>
2024-09-06 12:23:13 -0300

    Add files via upload

---
+++ dados_2.js
@@ -0,0 +1,80 @@
+let dados = [
+    {
+        titulo : "Atraxa, <br>Voz dos Pretores",
+        descricao : "Criatura lendária - Anjo Horror Phyrexiano<br> Voar, vigilância, toque mortífero, vinculo com a vida<br>No inicio de sua etapa final, prolifere. <br>4/4",
+        link : "https://www.ligamagic.com.br/?view=cards/card&card=Atraxa,%20Praetors%27%20Voice&aux=Atraxa,%20Voz%20dos%20Pretores",
+        imagem : "2xm-190-atraxa-praetors-voice.jpg",
+        tags: "Multicolorida lendaria"
+    },
+    {
+        titulo:"Drivnod, Dominus da Carnificina",
+        descricao:"Criatura Lendária — Phyrexiano Horror<br>Se a morte de uma criatura fizer com que uma habilidade desencadeada de uma permanente que você controla seja desencadeada, aquela habilidade será desencadeada uma vez adicional.<br>{B/P}{B/P}, exile três cards de criatura de seu cemitério: Coloque um marcador de indestrutível em Drivnod, Dominus da Carnificina.<br>8/3",
+        link:"https://www.ligamagic.com.br/?view=cards/card&card=Drivnod,%20Carnage%20Dominus&aux=Drivnod,%20Dominus%20da%20Carnificina",
+        imagem:"one-90-drivnod-carnage-dominus.jpg",
+        tags: "Preto lendaria"
+    },
+    {
+        titulo : "Elesh Norn, Mãe das Máquinas",
+        descricao : "Criatura Lendária — Phyrexiano Pretor<br>Vigilância<br>Se uma permanente que entrar no campo de batalha fizer com que uma habilidade desencadeada de uma permanente que você controla seja desencadeada, aquela habilidade será desencadeada uma vez adicional.<br>As permanentes que entram no campo de batalha não desencadeiam as habilidades de permanentes que seus oponentes controlam.<br>4/7",
+        link : "https://www.ligamagic.com.br/?view=cartas/card&card=Elesh%20Norn,%20Mother%20of%20Machines&",
+        imagem : "one-10-elesh-norn-mother-of-machines.jpg",
+        tags: "Branco lendaria"
+
+    },
+    {
+        titulo : "Jin-Gitaxias, Tirano do Progresso",
+        descricao : "Criatura Lendária — Pretor Phyrexiano<br>Toda vez que você conjurar uma mágica instantânea, um artefato ou um feitiço, copie aquela mágica. Você pode escolher novos alvos para a cópia. Esta habilidade é desencadeada apenas uma vez a cada turno.<br>Toda vez que um oponente conjurar uma mágica instantânea, um artefato ou um feitiço, anule aquela mágica. Esta habilidade é desencadeada apenas uma vez a cada turno.<br>5/5",
+        link : "https://www.ligamagic.com.br/?view=cartas/card&card=Jin-Gitaxias,%20Progress%20Tyrant&",
+        imagem : "neo-59-jin-gitaxias-progress-tyrant.jpg",
+        tags : "Azul lendaria"
+    }, 
+    {
+        titulo:"Mondrak, Domina da Glória",
+        descricao:"Criatura Lendária — Phyrexiano Horror<br>Se uma ou mais fichas seriam criadas sob seu controle, em vez disso, cria-se o dobro.<br>{1}{W/P}{W/P}, sacrifique dois outros artefatos e/ou criaturas: Coloque um marcador de indestrutível em Mondrak, Domina da Glória.<br>4/4",
+        link:"https://www.ligamagic.com.br/?view=cards/card&card=Mondrak,%20Glory%20Dominus&aux=Mondrak,%20Domina%20da%20Glória",
+        imagem:"one-23-mondrak-glory-dominus.jpg",
+        tags: "Branco lendaria"
+    },
+    {
+        titulo : "Sheoldred, o Apocalipse",
+        descricao : "Criatura lendária - Phyrexiano Pretor<br> Toque mortífero<br> Toda vez que você compra um card, você ganha 2 pontos de vida.<br>Toda vez que um oponente compra um card, ele perde 2 pontos de vida.<br>4/5",
+        link : "https://www.ligamagic.com.br/?view=cards/card&card=Sheoldred%2C+the+Apocalypse",              
+        imagem : "dmu-107-sheoldred-the-apocalypse.jpg",
+        tags: "Preto lendaria"
+    },
+    {
+        titulo:"Solphim, Domina do Pandemônio",
+        descricao:"Criatura Lendária — Phyrexiano Horror<br>Se uma fonte que você controla causaria dano não de combate a um oponente ou a uma permanente que um oponente controla, em vez disso, ela causa o dobro daquele dano àquele jogador ou àquela permanente.<br>{1}{R/P}{R/P}, descarte dois cards: Coloque um marcador de indestrutível em Solphim, Domina do Pandemônio.<br>5/4",
+        link:"https://www.ligamagic.com.br/?view=cards/card&card=Solphim,%20Mayhem%20Dominus&aux=Solphim,%20Domina%20do%20Pandemônio",
+        imagem:"one-150-solphim-mayhem-dominus.jpg",
+        tags: "Vermelho lendaria"
+    },
+    {
+        titulo : "Tekuthal, Dominus do Inquérito",
+        descricao : "Criatura lendária - Phyrexiano Horror<br>Voar<br>Se você proliferaria, em vez disso, prolifere duas vezes.<br>Pague {1}{U/P}{U/P}, remova três marcadores de outros artefatos, criaturas e planeswalkers que você controla: Coloque um marcador de indestrutivel em Tekuthal, Dominus do Inquérito.<br>3/5",
+        link : "https://www.ligamagic.com.br/?view=cards/card&card=Tekuthal%2C+Inquiry+Dominus",
+        imagem : "one-71-tekuthal-inquiry-dominus.jpg",
+        tags: "Azul lendaria"
+    },
+    {
+        titulo:"Urabrask, Pretor Herege",
+        descricao:"Criatura Lendária — Pretor Phyrexiano<br>Ímpeto<br>No início de sua manutenção, exile o card do topo de seu grimório. Você pode jogá-lo neste turno.<br>No início da manutenção de cada oponente, na próxima vez em que ele compraria um card neste turno, em vez disso, ele exila o card do topo do próprio grimório. Ele pode jogá-lo neste turno.<br>4/4",
+        link:"https://www.ligamagic.com.br/?view=cartas/card&card=Urabrask,%20Heretic%20Praetor&",
+        imagem:"snc-129-urabrask-heretic-praetor.jpg",
+        tags: "Vermelho lendaria"
+    },
+    {
+        titulo:"Vorinclex, Salteador Monstruoso",
+        descricao:"Criatura Lendária — Pretor Phyrexiano<br>Atropelar, ímpeto<br>Se você for colocar um ou mais marcadores em uma permanente ou um jogador, em vez disso, coloque o dobro de cada um daqueles tipos naquela permanente ou naquele jogador.<br>Se um oponente for colocar um ou mais marcadores em uma permanente ou um jogador, em vez disso, ele coloca a metade de cada um daqueles tipos naquela permanente ou naquele jogador, arredondada para baixo.<br>6/6",
+        link:"https://www.ligamagic.com.br/?view=cartas/card&card=Vorinclex,%20Monstrous%20Raider&",
+        imagem:"khm-199-vorinclex-monstrous-raider.jpg",
+        tags: "Verde lendaria"
+    },
+    {
+        titulo:"Zopandrel, Domina da Fome",
+        descricao:"Criatura Lendária — Phyrexiano Horror<br>Alcance<br>No início de cada combate, dobre o poder e a resistência de cada criatura que você controla até o final do turno.<br>{G/P}{G/P}, sacrifique duas outras criaturas: Coloque um marcador de indestrutível em Zopandrel, Domina da Fome.<br>4/6",
+        link:"https://www.ligamagic.com.br/?view=cards/card&card=Zopandrel,%20Hunger%20Dominus&aux=Zopandrel,%20Domina%20da%20Fome",
+        imagem:"one-195-zopandrel-hunger-dominus.jpg",
+        tags: "Verde lendaria"
+    }   
+];
